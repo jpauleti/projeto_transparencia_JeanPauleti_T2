a68fd819b8c39a623006c6a029a74f026cdb4709
jpauleti <jpauleti@gmail.com>
2026-07-12 19:09:59 -0300

    cria schema transparencia

---
+++ codigos/0_criar_banco.sql
@@ -0,0 +1,13 @@
+-- PROJETO: Transparência em Viagens a Serviço
+-- AUTOR: Jean Pauleti
+-- TURMA: T2
+-- SGBD: MySQL
+-- SCHEMA: transparencia
+
+DROP DATABASE IF EXISTS transparencia;
+
+CREATE DATABASE transparencia
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+USE transparencia;
